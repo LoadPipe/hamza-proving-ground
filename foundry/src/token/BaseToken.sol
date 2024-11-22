2240317af42aa2a51c6e34e6bca067328fd8c98e
Tronky <john.kosinski@gmail.com>
2024-11-22 22:59:04 +0700

    base token

---
+++ foundry/src/token/BaseToken.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+    constructor() ERC20("Hamza Base Token", "HAMZ") {}
+}
