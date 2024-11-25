0e0c375aa45cc64979f0f796514fefe7c39eac0e
Tronky <john.kosinski@gmail.com>
2024-11-25 21:44:49 +0700

    abis in rest-api

---
+++ rest-api/src/abi/index.ts
@@ -0,0 +1,54 @@
+export const accountIndex = [
+    {
+        type: 'function',
+        name: 'accountExists',
+        inputs: [{ name: 'addr', type: 'address', internalType: 'address' }],
+        outputs: [{ name: '', type: 'bool', internalType: 'bool' }],
+        stateMutability: 'view',
+    },
+    {
+        type: 'function',
+        name: 'createAccount',
+        inputs: [
+            { name: 'id', type: 'bytes32', internalType: 'bytes32' },
+            { name: 'userEoa', type: 'address', internalType: 'address' },
+        ],
+        outputs: [],
+        stateMutability: 'nonpayable',
+    },
+    {
+        type: 'event',
+        name: 'AccountCreated',
+        inputs: [
+            {
+                name: 'accountAddress',
+                type: 'address',
+                indexed: true,
+                internalType: 'address',
+            },
+        ],
+        anonymous: false,
+    },
+];
+
+export const smartAccount = [
+    {
+        type: 'constructor',
+        inputs: [
+            { name: '_bundler', type: 'address', internalType: 'address' },
+            { name: '_id', type: 'bytes32', internalType: 'bytes32' },
+            { name: '_userEoa', type: 'address', internalType: 'address' },
+        ],
+        stateMutability: 'nonpayable',
+    },
+    {
+        type: 'function',
+        name: 'withdrawFunds',
+        inputs: [
+            { name: 'amount', type: 'uint256', internalType: 'uint256' },
+            { name: 'tokenAddress', type: 'address', internalType: 'address' },
+        ],
+        outputs: [],
+        stateMutability: 'nonpayable',
+    },
+];
