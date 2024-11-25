aacbe1b1a87b179ef0d8f44b93afe32f9d28198b
Tronky <john.kosinski@gmail.com>
2024-11-25 21:08:15 +0700

    added route and placeholders for account creation

--- rest-api/src/controllers/account.controller.ts
+++ rest-api/src/controllers/account.controller.ts
@@ -10,4 +10,13 @@ import { AccountService } from '../services/account.service';
 @Controller('account')
 export class AccountController {
     constructor(private readonly accountService: AccountService) {}
+
+    @Get('/create') // or @Get('/exch') depending on your setup
+    async createAccount(@Query('username') username: string): Promise<{
+        paymentAddress: string;
+        accountAddress: string;
+        accountChainId: number;
+    }> {
+        return await this.accountService.createAccount(username);
+    }
 }
--- rest-api/src/services/account.service.ts
+++ rest-api/src/services/account.service.ts
@@ -5,4 +5,27 @@ import { HttpServer, Injectable } from '@nestjs/common';
 //TODO: rename
 export class AccountService {
     constructor(private readonly httpService: HttpService) {}
+
+    async createAccount(username: string): Promise<{
+        paymentAddress: string;
+        accountAddress: string;
+        accountChainId: number;
+    }> {
+        //
+        //check the smart account index to see if the account (by username) already exists
+        //
+        //if it exists already, return an error
+        //
+        //create a payment address, save the private key
+        //
+        //create an account on the smart contract
+        //
+        //return the payment address and smart account address
+
+        return {
+            paymentAddress: '',
+            accountAddress: '',
+            accountChainId: 0,
+        };
+    }
 }
