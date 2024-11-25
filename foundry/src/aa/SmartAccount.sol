d89a73c51ddb629759417014ad5d88c9fc63c5f4
Tronky <john.kosinski@gmail.com>
2024-11-25 20:20:03 +0700

    allow user EOA to withdraw funds

--- foundry/src/aa/SmartAccount.sol
+++ foundry/src/aa/SmartAccount.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.7;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract SmartAccount { 
     bytes32 id; 
     address bundler;
@@ -10,5 +12,19 @@ contract SmartAccount {
         bundler = _bundler;
         id = _id;
         userEoa = _userEoa;
+    }
+
+    function withdrawFunds(uint256 amount, address tokenAddress) external {
+        if (msg.sender != userEoa) {
+            revert("Unauthorized");
+        }
+
+        if (tokenAddress == address(0)) {
+            IERC20 token = IERC20(tokenAddress); 
+            token.transfer(userEoa, amount);
+        }
+        else {
+            payable(userEoa).transfer(amount);
+        }
     }
 }
